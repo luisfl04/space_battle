aae051cd903c139b614232daa518ab7f922d00d3
@luisfl04_ <luisfelipecontato08@gmail.com>
2024-12-27 15:51:12 -0300

    upd -> Tela de cadastro de username em versão funcional

--- index.c
+++ index.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "telas/tela_de_cadastro.c" 
+
 int main(int argc, char **argv){
     
+    // Iniciando tela que obtém o username do jogador:
+    iniciarTelaDeCadastro();
+
+
+
 
 
 
